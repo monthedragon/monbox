453879f6c582eaa6fb6a08b0685ad0d1ca57b915
monthedragon <mon_tuyay22@yahoo.com>
2023-04-26 22:56:35 +0800

    Adding new comment from PhpStrom under mon-branch

--- ExcelCodesSamples/basic.php
+++ ExcelCodesSamples/basic.php
@@ -5,7 +5,7 @@ $template_path = 'include/Excel/example/template/template.xlsx';
 $excel_xml = new Excel($template_path);
 $excel_xml->setFileName("excel_file_".time());
 
-//Sheet #1
+//Sheet #12345-this will be merged later in MAIN
 //rename active sheet
 $excel_xml->setActiveSheetName("1New Sheetname ".date("Y-m-d"));
 //sheet 1
